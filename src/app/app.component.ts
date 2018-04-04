346054484fa23241c7db953a88912abdc817fd32
Satit Rianpit <siteslave@msn.com>
2018-04-04 20:41:37 +0700

    add push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,38 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import { DashboardPage } from '../pages/dashboard/dashboard';
 import { LoginPage } from '../pages/login/login';
+import { Push, PushOptions, PushObject } from '@ionic-native/push';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(private push: Push, platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+
+      const options: PushOptions = {
+        android: {},
+        ios: {
+          alert: 'true',
+          badge: true,
+          sound: 'false'
+        },
+      };
+
+      const pushObject: PushObject = this.push.init(options);
+
+      pushObject.on('registration').subscribe((registration: any) => console.log('Device registered', registration));
+
+      pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
+      
+      pushObject.on('notification').subscribe((notification: any) => console.log('Received a notification', notification));
+    
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Push, PushObject, PushOptions } from '@ionic-native/push';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -41,6 +42,7 @@ import { LoginProvider } from '../providers/login/login';
   providers: [
     StatusBar,
     SplashScreen,
+    Push,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     MemberProvider,
     KpiProvider,
